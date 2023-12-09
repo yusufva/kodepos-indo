4ba325899903f4d8eb8ad1647ca9952db88f8e75
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-09 18:27:53 +0700

    refactor: adding try catch block

--- controller/kodepos.js
+++ controller/kodepos.js
@@ -4,44 +4,52 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
 exports.getKodePos = async (req, res) => {
-    // [kelurahan]-[kecamatan]-[kota]
-    const q = req.query.q
-    const query = q.replace('"', '')
-    const queryArray = q.split("-");
-    const kelurahan = queryArray[0];
-    const kecamatan = queryArray[1];
-    const kota = queryArray[2];
-    const provinceId = +req.params.id;
-    const kodepos = await prisma.db_postal_code_data.findFirst({
-        where:{
-            AND:[
-                {
-                    province_code:provinceId
-                },
-                {
-                    urban:kelurahan
-                },
-                {
-                    sub_district:kecamatan
-                },
-                {
-                    city:kota
-                }
-            ]
+    try{
+        // [kelurahan]-[kecamatan]-[kota]
+        const q = req.query.q
+        const query = q.replace('"', '')
+        const queryArray = q.split("-");
+        const kelurahan = queryArray[0];
+        const kecamatan = queryArray[1];
+        const kota = queryArray[2];
+        const provinceId = +req.params.id;
+        const kodepos = await prisma.db_postal_code_data.findFirst({
+            where:{
+                AND:[
+                    {
+                        province_code:provinceId
+                    },
+                    {
+                        urban:kelurahan
+                    },
+                    {
+                        sub_district:kecamatan
+                    },
+                    {
+                        city:kota
+                    }
+                ]
+            }
+        });
+        if (!kodepos) {
+            return res.status(404).json({
+                result: "failed",
+                statusCode: "404",
+                message: "data not found"
+            })
         }
-    });
-    if (!kodepos) {
-        return res.status(404).json({
+        kodepos.id = parseInt(kodepos.id);
+        return res.json({
+            result: "success",
+            statusCode: "200",
+            message: "data successfully fetched",
+            data: kodepos
+        });
+    } catch{
+        return res.status(400).json({
             result: "failed",
-            statusCode: "404",
-            message: "data not found"
+            statusCode: "400",
+            message: "Id must be a number"
         })
     }
-    kodepos.id = parseInt(kodepos.id);
-    return res.json({
-        result: "success",
-        statusCode: "200",
-        message: "data successfully fetched",
-        data: kodepos
-    });
 }
