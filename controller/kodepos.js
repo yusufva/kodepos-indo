453aab0a7b729cda11fbe7dd501599b9f48dfcdd
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-09 18:12:33 +0700

    fix: fixing anomali get data

--- controller/kodepos.js
+++ controller/kodepos.js
@@ -6,21 +6,32 @@ const prisma = new PrismaClient()
 exports.getKodePos = async (req, res) => {
     // [kelurahan]-[kecamatan]-[kota]
     const q = req.query.q
+    const query = q.replace('"', '')
     const queryArray = q.split("-");
     const kelurahan = queryArray[0];
     const kecamatan = queryArray[1];
     const kota = queryArray[2];
-    const provinceId = req.params.id;
+    const provinceId = +req.params.id;
     const kodepos = await prisma.db_postal_code_data.findFirst({
         where:{
-            urban: kelurahan,
-            sub_district: kecamatan,
-            city: kota,
-            province_code: +provinceId
+            AND:[
+                {
+                    province_code:provinceId
+                },
+                {
+                    urban:kelurahan
+                },
+                {
+                    sub_district:kecamatan
+                },
+                {
+                    city:kota
+                }
+            ]
         }
     });
     if (!kodepos) {
-        return res.json({
+        return res.status(404).json({
             result: "failed",
             statusCode: "404",
             message: "data not found"
